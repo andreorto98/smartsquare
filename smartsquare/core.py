372b4bc8682ffe3e51ed2b25bbbd7e38be3a24e1
andreorto98 <andrea.ortone98@gmail.com>
2020-10-19 16:56:49 +0200

    initial import

---
+++ smartsquare/core.py
@@ -0,0 +1,8 @@
+'''
+doc string describing the module
+
+'''
+
+def square(x):
+    ''' return the square of x '''
+    return x**2
---
+++ tests/test_core.py
@@ -0,0 +1,19 @@
+'''
+test for the core.py file
+
+'''
+
+import unittest             # prima i pacchetti std_in, poi gli altri, poi dopo una riga i miei pacchetti
+
+from smartsquare.core import square
+#export PYTHONPATH=/Users/andrea/Desktop/computing_methods/smartsquare/  per farlo funzionare...
+
+
+class TestCore(unittest.TestCase):      # classes con CamelCase
+    ''' Unittest for core module '''
+    def test_float(self):                   # Metodi/Funzioni con '_'
+        '''test
+        self.assertAlmostEqual(square(2.), 4.)
+
+if __name__ == '__main__':
+    unittest.main()
